793f7fba59c0606520a1560363f0198aa16c07ee
Avi Aryan <avi.aryan123@gmail.com>
2017-12-14 19:44:15 +0530

    use ractive to show user data

--- timemanager/static/dashboard.js
+++ timemanager/static/dashboard.js
@@ -1,9 +1,45 @@
+var ractive;
+var token;
+var userObj;
+
 $(document).ready(function(){
 	checkLogin();
 	$('#logoutButton').click(handleLogout);
+	token = getCookie('token');
+	// load data
+	loadData();
 });
 
 function handleLogout(){
 	deleteCookie('token');
 	checkLogin();
+}
+
+function loadData(){
+	ractive = new Ractive({
+	  target: '#target',
+	  template: '#template',
+	  data: { name: 'User' }
+	});
+
+	getUserInfo();
+}
+
+function getUserInfo(){
+	$.ajax({
+		type: 'GET',
+		url: '/api/v1/users/user',
+		dataType: 'json',
+		beforeSend: function(request) {
+	    request.setRequestHeader("Authorization", 'Bearer ' + token);
+	  },
+		success: function(resp){
+			userObj = resp;
+			var name = resp['fullname'] || resp['username'];
+			ractive.set('name', name);
+		},
+		error: function(xhr, status, error){
+			console.log(xhr.responseJSON['message']);
+		}
+	});
 }

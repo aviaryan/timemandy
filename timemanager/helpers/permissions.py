39c68eb7492d96ca1bd790d0831f805206226778
Avi Aryan <avi.aryan123@gmail.com>
2017-12-14 13:56:14 +0530

    create basic task crud API

--- timemanager/helpers/permissions.py
+++ timemanager/helpers/permissions.py
@@ -1,13 +1,14 @@
 from functools import wraps
-from flask_login import current_user
+from flask import g
 from .errors import PermissionDeniedError
 from timemanager.models.user_model import User
+from timemanager.models.task_model import Task
 
 
 def staff_only(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        user = current_user
+        user = g.current_user
         is_user_valid = user and (user.is_admin or user.is_manager)
         if is_user_valid:
             return func(*args, **kwargs)
@@ -16,10 +17,22 @@ def staff_only(func):
     return wrapper
 
 
+def admin_only(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = g.current_user
+        is_user_valid = user and user.is_admin
+        if is_user_valid:
+            return func(*args, **kwargs)
+        else:
+            raise PermissionDeniedError(message='Admin account required to access this feature')
+    return wrapper
+
+
 def has_user_access(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        user = current_user
+        user = g.current_user
         target_user_id = kwargs.get('user_id')
         if target_user_id is None:
             return func(*args, **kwargs)
@@ -27,10 +40,28 @@ def has_user_access(func):
 
         if user.is_admin:  # all good
             return func(*args, **kwargs)
-        if user.is_manager and (not current_user.is_manager and not current_user.is_admin):  # manager case
+        if user.is_manager and (not g.current_user.is_manager and not g.current_user.is_admin):  # manager case
             return func(*args, **kwargs)
         if user.id == target_user.id:
             return func(*args, **kwargs)
         # default
         raise PermissionDeniedError(message='You don\'t have access to this account')
+    return wrapper
+
+
+def has_task_access(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = g.current_user
+        target_task_id = kwargs.get('task_id')
+        if target_task_id is None:
+            return func(*args, **kwargs)
+        target_task = Task.query.get(target_task_id)
+
+        if user.is_admin:  # all good
+            return func(*args, **kwargs)
+        if user.id == target_task.user_id:  # normal access
+            return func(*args, **kwargs)
+        # default
+        raise PermissionDeniedError(message='You don\'t have access to this task')
     return wrapper
--- timemanager/views/task_api.py
+++ timemanager/views/task_api.py
@@ -1,7 +1,84 @@
-from flask_restplus import Namespace  #, Resource, fields
+from flask_restplus import Namespace, Resource, fields
+from flask_login import login_required
+from flask import g
 
-from timemanager.models.task_model import Task as TaskModel  # noqa
+from timemanager.models.task_model import Task as TaskModel
 
+from timemanager.helpers.dao import BaseDAO
+from timemanager.helpers.utils import AUTH_HEADER_DEFN
+from timemanager.helpers.permissions import has_task_access, admin_only
 
 
 api = Namespace('tasks', description='Tasks', path='/')  # noqa
+
+TASK = api.model('Task', {
+    'id': fields.Integer(required=True),
+    'title': fields.String(required=True),
+    'minutes': fields.Integer(required=True),
+    'date': fields.DateTime(required=True),
+    'comments': fields.String(),
+    'user_id': fields.Integer()
+})
+
+TASK_POST = api.clone('TaskPost', TASK, {})
+del TASK_POST['id']
+
+
+class TaskDAO(BaseDAO):
+    pass
+
+
+DAO = TaskDAO(TaskModel, TASK_POST)
+
+
+@api.route('/tasks/<int:task_id>')
+class Task(Resource):
+    @login_required
+    @has_task_access
+    @api.header(*AUTH_HEADER_DEFN)
+    @api.doc('get_user')
+    @api.marshal_with(TASK)
+    def get(self, task_id):
+        """Fetch a task given its id"""
+        return DAO.get(task_id)
+
+    @login_required
+    @has_task_access
+    @api.header(*AUTH_HEADER_DEFN)
+    @api.doc('update_task')
+    @api.marshal_with(TASK)
+    @api.expect(TASK_POST)
+    def put(self, task_id):
+        """Update a task given its id"""
+        return DAO.update(task_id, self.api.payload)
+
+    @login_required
+    @has_task_access
+    @api.header(*AUTH_HEADER_DEFN)
+    @api.doc('delete_task')
+    @api.marshal_with(TASK)
+    def delete(self, task_id):
+        """Delete a task given its id"""
+        return DAO.delete(task_id, user_id=None)  # has_task_access already checks this
+
+
+@api.route('/task')
+class TaskList(Resource):
+    @api.header(*AUTH_HEADER_DEFN)
+    @login_required
+    @api.doc('create_task')
+    @api.marshal_with(TASK)
+    @api.expect(TASK_POST)
+    def post(self):
+        """Create a task"""
+        return DAO.create(self.api.payload, user_id=g.current_user.id)
+
+    @api.header(*AUTH_HEADER_DEFN)
+    @login_required
+    @admin_only
+    @api.doc('list_tasks')
+    @api.marshal_list_with(TASK)
+    def get(self):
+        """List all tasks"""
+        # TODO: admin only, also think about levels
+        return DAO.list()
